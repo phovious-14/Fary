e6b48a037493325884c1ddabb5ae65f454191bbe
phovious-14 <krishnpanchal14@gmail.com>
2025-05-17 11:27:27 +0530

    Remove unused metadata generation and frame configuration from UserPage component to streamline the codebase. This change eliminates unnecessary imports and simplifies the structure, enhancing maintainability.

--- app/user/[walletAddress]/[fid]/page.tsx
+++ app/user/[walletAddress]/[fid]/page.tsx
@@ -1,38 +1,5 @@
 import UserStoriesPage from "./UserStory";
 
-import { env } from "@/lib/env";
-import { Metadata } from "next";
-
-const appUrl = env.NEXT_PUBLIC_URL;
-
-const frame = {
-  version: "next",
-  imageUrl: `${appUrl}/images/feed.png`,
-  button: {
-    title: "Launch App",
-    action: {
-      type: "launch_frame",
-      name: "Mini-app Starter",
-      url: appUrl,
-      splashImageUrl: `${appUrl}/images/splash.png`,
-      splashBackgroundColor: "#ffffff",
-    },
-  },
-};
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Mini-app Starter",
-    openGraph: {
-      title: "Mini-app Starter",
-      description: "A starter for Farcastermini-apps",
-    },
-    other: {
-      "fc:frame": JSON.stringify(frame),
-    },
-  };
-}
-
 export default function UserPage({
   params,
 }: {
